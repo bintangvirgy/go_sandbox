9c48786f4d6e5d97a6d3fdd8f99b1cbe70d433a1
Bintang Virgy <bintangvirgy@gmail.com>
2021-07-31 23:16:05 +0700

    problem 5

--- 00_project_euler/project_euler.go
+++ 00_project_euler/project_euler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 // this is code for solving project euler in projecteuler.net
@@ -26,15 +27,73 @@ func main() {
 	// fmt.Println(vres)
 
 	// problem 4
-	vpalindrome := prob4()
-	fmt.Println(vpalindrome)
+	// vpalindrome := prob4()
+	// fmt.Println(vpalindrome)
+
+	// problem 5
+	vkpk := prob5()
+	fmt.Println(vkpk)
 
 }
 
+func prob5() int {
+	res := 0
+	num := 20
+	for i := 1; true; i++ {
+		res = num * i
+		for j := num; j > 0; j-- {
+			if res%j != 0 {
+				break
+			}
+
+			if j == 1 {
+				return res
+			}
+		}
+	}
+	return res
+}
+
 func prob4() int {
+	// largest, so we start from here
+	first_num := 999
 	res := 0
 
+	// loop backward for first number
+	for i := first_num; i > 0; i-- {
+		// loop backward too for second number
+		// but i make limit by hundred place from first number, so if we still calculate 9xx from first number, we can't make second number lower than 9xx
+		for j := i; j/100 >= i/100; j-- {
+			res = i * j
+			// check palindrome use custom function, if palindrome then break
+			if isPalindrome(&res) {
+				break
+			}
+		}
+		if isPalindrome(&res) {
+			break
+		}
+	}
 	return res
+
+}
+
+func isPalindrome(num *int) bool {
+	str := strconv.Itoa(*num)
+	len_str := len(str) - 1
+	for i := 0; i < len(str); i++ {
+		if i >= len_str {
+			break
+		}
+
+		if string(str[i]) != string(str[len_str]) {
+			return false
+		}
+
+		len_str--
+
+	}
+	return true
 
 }
 
